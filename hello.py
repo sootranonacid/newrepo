99e8b1600ccdff8a4393ef6c262260d81ac45b88
sootranonacid <sootranonacid@gmail.com>
2024-09-17 13:31:55 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
 # hello_world.py
 
-def main():
-    print("Hello, World! Nowww")
-    print("hello again")
-    print("Hello, World!")
-    print("hello again")
-
-if __name__ == "__main__":
-    main()
+print("Hello, World! Nowww")
+  
